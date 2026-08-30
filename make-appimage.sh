875ab534aa95189a11c0253ab912d29d53c8a4bf
Link4Electronics <link4electronics@gmail.com>
2026-08-30 05:30:37 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -25,3 +25,7 @@ echo 'export ANYLINUX_EXECVE_WRAP_PATHS="$DATADIR"' >> ./AppDir/bin/execve-wrap-
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
